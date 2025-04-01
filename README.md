1a0581aea9dca5c073ba1de42d4c7e032d33e929
cindy1408 <cindycheung1408@gmail.com>
2025-04-01 17:03:03 +0100

    add readme autoload -Uz compinit export PATH="$GOPATH/bin:$PATH"

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # lotsoflovecindy
 
 Step 1: 
-1. store images 
+1. store images in gcs (done)
+2. retrieve images from gcs (done)
+3. order them in order by date (latest first)
+4. option to retrieve videos as well from gcs
+5. add tests 
+6. ability to add descriptions and when to post to each images
+7. when an image is clicked, the image enlarges with the description and date to post 
--- ui/src/App.js
+++ ui/src/App.js
@@ -56,15 +56,19 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img className="profile" src="/data/img/profile.jpg" alt="Profile"/>
-                <div>
-                    <h2>Lots of Love, Cindy</h2>
-                    <h3>bio: hello this is a test haha </h3>
+                <div className="header">
+                    <img className="profile" src="/data/img/profile.jpg" alt="Profile"/>
+                    <div>
+                        <h2>Lots of Love, Cindy</h2>
+                        <h3>bio: hello this is a test haha </h3>
+                    </div>
                 </div>
+
+                <div className="upload-section">
                 <input type="file" id="fileInput" onChange={handleFileChange} />
                 <button onClick={uploadFile}>Upload</button> {/* Call uploadFile on button click */}
+                </div>
 
-                {/* Image Gallery */}
                 <div className="image-gallery">
                     {images.length > 0 ? (
                         images.map((url, index) => (
