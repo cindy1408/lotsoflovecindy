97673f01af930deb952d0b348bbf09e673ef2fbc
cindy1408 <cindycheung1408@gmail.com>
2025-04-09 10:44:54 +0100

    clean up

--- backend/handlers/posts.go
+++ backend/handlers/posts.go
@@ -9,10 +9,9 @@ import (
 	"lotsoflovecindy/m/v2/respositories"
 )
 
-// HTTP handlers that use the repository functions
+// RetrieveHandler HTTP handlers that use the repository functions
 func RetrieveHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("HERRRREEEE!!!!")
 		posts, err := respositories.GetAllPosts(db)
 		if err != nil {
 			log.Printf("Failed to get posts: %v", err)
@@ -20,13 +19,11 @@ func RetrieveHandler(db *gorm.DB) http.HandlerFunc {
 			return
 		}
 
-		log.Println("HERRRREEEE")
 		var fileURLs []string
 		for _, post := range posts {
 			fileURLs = append(fileURLs, post.ContentURL)
 		}
 
-		log.Println("file url: ", fileURLs)
 		w.Header().Set("Content-Type", "application/json")
 		err = json.NewEncoder(w).Encode(fileURLs)
 		if err != nil {
