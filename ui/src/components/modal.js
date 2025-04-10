842b13321a823ebf35948ae529ea0515fe964d96
cindy1408 <cindycheung1408@gmail.com>
2025-04-10 11:55:51 +0100

    close modal when clicked outside

--- ui/src/components/modal.js
+++ ui/src/components/modal.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Modal({ image, description, onClose }) {
+function ImageModal({ image, description, onClose }) {
     if (!image) return null;
 
+    const handleBackgroundClick = (event) => {
+        if (event.target.className === "modal") {
+            onClose();
+        }
+    };
+
     return (
-        <div className="modal">
+        <div className="modal" onClick={handleBackgroundClick}>
             <div className="modal-content">
                 <span className="close" onClick={onClose}>&times;</span>
                 <img src={image} alt="Enlarged view" className="enlarged-img" />
@@ -14,4 +20,4 @@ function Modal({ image, description, onClose }) {
     );
 }
 
-export default Modal;
+export default ImageModal;
